b5a4277cc572a2dc5e5842e25393c2336bd34999
Tinnagame <98tinnagame>
2021-07-30 04:36:49 +0700

    4

--- src/router/index.js
+++ src/router/index.js
@@ -83,7 +83,13 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,scrollBehavior(to, from, savedPosition) {
+    if(savedPosition) {
+      return savedPosition
+    } else {
+      return {top:0}
+    }
+  }
 })
 router.beforeEach(() => {
   NProgress.start()
