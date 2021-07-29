4fd6cf535a3aa5118eede37c562c8e4169283d21
Tinnagame <98tinnagame>
2021-07-30 02:23:22 +0700

    edit btn

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,24 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Click Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      this.GStore.flashMessage = 
+      'Data has been updated '
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      },3000)
+      this.$router.push({
+        name:'EventDetails',
+        params: {id: this.event.id}
+      })
+    }
+  }
 }
 </script>
